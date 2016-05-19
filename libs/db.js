677f3981778db16e7bce5d1eac72d85f86ff1535
yenchulin <103306003@nccu.edu.tw>
2016-05-19 10:37:17 +0800

    first

--- libs/db.js
+++ libs/db.js
@@ -3,9 +3,9 @@ var knex = require('knex')({
   client: 'mysql',
   connection: {
     host     : '127.0.0.1',
-    port     :  8889,
+    port     :  3306,
     user     : 'root',
-    password : 'root',
+    password : '',
     database : 'blog'
   },
   pool: {
